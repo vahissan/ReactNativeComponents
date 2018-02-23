83095446fd5cae4f803ac48c3c27c8c80b018d9a
Vahissan Nandakumar <email@vahissan.com>
2018-02-23 16:05:02 -0500

    Basic material design button with raised and flat variants

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import MaterialButton from './components/material-button';
+
+export default class App extends Component {
+
+  onBtnPress() {
+    console.log('btn pressed');
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MaterialButton 
+          disabled={false}
+          flat={false}
+          text="Button" 
+          onPress={this.onBtnPress} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  btn: {
+    backgroundColor: 'red'
+  }
+});
